ae79c373502b46891f6f81b1f3c0e9df56da5c57
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-04-04 13:46:01 +0100

    Typo in jar

--- ci/test.sh
+++ ci/test.sh
@@ -1,3 +1,3 @@
-mkdir build
-javac -classpath .:ci/junit-jplatform-console-standalone-1.9.2.jar -d build src/rasterise/*.java
-java -jar ci/junit-jplatform-console-standalone-1.9.2.jar --class-path build --scan-class-path
+mkdir -p build
+javac -classpath .:ci/junit-platform-console-standalone-1.9.2.jar -d build src/rasterise/*.java
+java -jar ci/junit-platform-console-standalone-1.9.2.jar --class-path build --scan-class-path
